746cc05998bb317493466315a93f85f8a79fac12
Isaac Jordan kimsey <jordankimsey@gmail.com>
2022-11-11 15:55:43 -0500

    Project setup

--- next.config.js
+++ next.config.js
@@ -2,6 +2,8 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  // app directory is still an experimental feature, so we need to set a flag in the next.config.js file in order to use it:
+  //experimental: { appDir: true },
+};
 
 module.exports = nextConfig
